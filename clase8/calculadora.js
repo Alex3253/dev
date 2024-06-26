db13d9fbb0f7a9916754f7f9e1932eeddf44154a
Alex3253 <152949338+Alex3253@users.noreply.github.com>
2024-06-26 10:38:17 -0500

    Add files via upload

---
+++ clase8/calculadora.js
@@ -0,0 +1,32 @@
+let value1 = 0;
+let value2 = 0;
+let operation = "suma"; // por defecto, la operacion e suma
+
+function setValue(variable, value){
+    if(variable === 1){
+        value1 = value;
+        document.getElementById('selectedValue1').textContent = value1; 
+    } else if (variable === 2){
+        value2 = value;  
+        document.getElementById('selectedValue2').textContent = value2;
+    }
+}
+
+
+function setOperation(op){
+    operation = op;
+    document.getElementById('selectedOperation').textContent =
+    (operation === 'sum') ? 'Suma' : 'Resta';
+
+}
+
+function calculateResult(){
+    let result;
+    if(operation ==='sum'){
+        result = value1 + value2;
+    } else if (operation ==='subtract'){ ///
+        result = value1 - value2;
+    }
+
+    document.getElementById('result').textContent = result;
+}
